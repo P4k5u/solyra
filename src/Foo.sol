581d5dec73f5badcb7744bb360b9aa0a8ff6217c
paksu <paksu39@icloud.com>
2025-01-13 15:37:05 +0800

    handle openzeppelin

--- src/Foo.sol
+++ src/Foo.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.28;
 
-contract Foo {
-    function id(uint256 value) external pure returns (uint256) {
-        return value;
+import { ERC721URIStorage, ERC721 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Foo is ERC721URIStorage {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("GameItem", "ITM") { }
+
+    function awardItem(address player, string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = _tokenIdCounter;
+        _safeMint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        _tokenIdCounter++;
+        return newItemId;
     }
 }
